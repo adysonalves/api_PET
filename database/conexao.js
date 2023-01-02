b039b8807e253df25a637a7e57fc032db9033b34
“Adyson <adyson6@gmail.com>
2023-01-02 10:41:13 -0300

    Configuração da conexão com BD

--- database/conexao.js
+++ database/conexao.js
@@ -1,11 +1,11 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize({
-    database: 'api',
-    username: 'root',
-    password: '1234',
-    dialect: 'mysql',
-    host: 'localhost',
-    port: '3306'
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT
 });
 
 
